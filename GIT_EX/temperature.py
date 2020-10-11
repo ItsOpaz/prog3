b50c5c196200202ae191170d7b49681cefa122f2
Pete <olli.pettersson@tuni.fi>
2020-10-11 12:57:26 +0300

    direction_query

--- GIT_EX/temperature.py
+++ GIT_EX/temperature.py
@@ -2,21 +2,24 @@
 def main():
 
     try:
+	input_type = input("Select transform direction (CF) or (FC): "
         input_temp = float(input("Input temperature: "))
     except:
         print("Not valid input")
         exit()
 
 
-    print("{} C = {} F".format(input_temp, t_transform(input_temp, 'c')))
+    t_transform(input_temp, input_type)))
 
 
 
 
 def t_transform(temp, type):
 
-    if type == 'c':
-        return float(temp * 9/5 + 32)
+    if type == 'CF':
+        print("{} C = {} F".format(temp, float(temp * 9/5 + 32)))
+    if type == 'FC':
+        print("{} F = {} C".format(temp, float((temp - 32) * 5/9)))
 
     return None
 
